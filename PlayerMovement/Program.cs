2640dffb40d9bb13ef5c869925888507b1ee41ac
Francisco Nguyen <francisconguyen@web.de>
2024-05-01 09:02:21 +0200

    Projektdateien hinzufügen.

---
+++ PlayerMovement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("A: Your total comes to $36.78.");
+        Console.WriteLine("B: Put it on my VISA.");
+        Console.WriteLine("A: There's a problem with your card.");
+        Console.WriteLine("B: What's wrong with it?");
+        Console.WriteLine("A: I believe it was declined.");
+        Console.WriteLine("B: You're kidding me!");
+        Console.WriteLine("A: Do you have another card I could try?");
+        Console.WriteLine("B: I only have one card.");
+        Console.WriteLine("A: Do you have cash, perhaps?");
+        Console.WriteLine("B: I don't have any cash on me. ");
+        Console.WriteLine("A : You're not going to be able to take these items today.");
+        Console.WriteLine(" B :I'll be back tomorrow.");
+        Console.ReadKey
+    }
